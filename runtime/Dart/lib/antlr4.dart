704e23aa2fa8f36abaa1ee58c002032d9a8d79e8
Patrick Allwood <Patrick.Allwood@rocksolidknowledge.com>
2021-07-09 11:19:32 +0100

    export additional types

--- runtime/Dart/lib/antlr4.dart
+++ runtime/Dart/lib/antlr4.dart
@@ -20,3 +20,5 @@ export 'src/vocabulary.dart';
 export 'src/runtime_meta_data.dart';
 export 'src/token.dart';
 export 'src/prediction_context.dart';
+export 'src/recognizer.dart';
+export 'src/interval_set.dart';
